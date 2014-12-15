9368be02d64d07408dc50ce8e12735d1f0d17905
Sven Lito <me@svenlito.com>
2014-12-15 23:16:22 +0100

    test(setup): meh

--- test/index.js
+++ test/index.js
@@ -1,34 +1,36 @@
- 'use strict';
+/* global require, describe, before, it*/
+
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
 var couchr = require('couchr');
 var async = require('async');
 var url = require('url');
+var expect = require('expect.js');
 
 var ChangesFeed = require('../');
+var host = 'http://127.0.0.1:5984';
+
+var feed = new ChangesFeed({
+  url: host
+});
 
 describe('all dbs changes feed', function () {
 
   before(function (done) {
-    var self = this;
-    var host = 'http://127.0.0.1:5984';
-
-
     this.db_path = url.resolve(host, 'a');
 
     async.series([
       async.apply(couchr.put, this.db_path)
     ],
-    function (err, res) {
+    function () {
       done();
     });
-
   });
 
-  beforeEach(function () {
-  });
-
-  it('set up listener', function (done) {
-
+  it('should expose a readable stream', function () {
+    expect(feed.readable).to.be.ok();
   });
 
 });
