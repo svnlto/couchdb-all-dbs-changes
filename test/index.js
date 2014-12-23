001dddf99ff648e5a8510491a215844165eb0428
Sven Lito <me@svenlito.com>
2014-12-23 23:11:36 +0100

    chore(wip): tests

--- test/index.js
+++ test/index.js
@@ -15,32 +15,26 @@ var host = 'http://127.0.0.1:5984';
 describe('all dbs changes feed', function () {
 
   before(function (done) {
-    var self = this;
-
     this.db_path = url.resolve(host, 'a');
-
-    this.change = null;
-    this.error = null;
 
     async.series([
       async.apply(couchr.put, this.db_path)
     ],
     function () {
-      self.feed = new ChangesFeed({
+
+      this.feed = new ChangesFeed({
         url: host
       });
 
-      done();
-    });
+      setTimeout(function () {
+        done();
+      }, 1000);
+    }.bind(this));
   });
 
-  beforeEach(function () {
-    this.feed.on('change', function (change) {
-      this.change = change;
-    }.bind(this));
-
-    this.feed.on('error', function (error) {
-      this.error = error;
+  after(function (done) {
+    couchr.del(this.db_path, function () {
+     done();
     });
   });
 
@@ -52,18 +46,18 @@ describe('all dbs changes feed', function () {
     expect(this.feed.on).to.be.ok();
   });
 
-  it('should post a doc and trigger a change on the feed', function (done) {
-      couchr.post(this.db_path, {
-        name: 'test_doc'
-      }, function (err, resp) {
-        console.log(this.change, this.error);
-        done();
-      }.bind(this));
-  });
+  it('should receive inserted doc through feed', function (done) {
+    var doc = {
+      name: 'test_doc'
+    };
 
-  it('should', function (done) {
-    this.timeout(5000);
-    done();
+    couchr.post(this.db_path, doc, function () {
+      this.feed.on('change', function (a) {
+        expect(a.name).to.eql('a');
+        expect(a.data.name).to.eql(doc.name);
+        done();
+      });
+    }.bind(this));
   });
 
 });
